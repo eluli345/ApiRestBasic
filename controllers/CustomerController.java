2d064c179a9fa58cc795faac800cd6ab88be09bc
eluli345 <ulisesbazzaro18@gmail.com>
2024-05-23 13:26:02 -0300

    commit

---
+++ controllers/CustomerController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+import com.example.demo.domain.Customer;
+
+@RestController
+public class CustomerController {
+	
+	private List<Customer> customers = new ArrayList<>(Arrays.asList(
+				new Customer(01, "Ana", "Cleta", "1234"),
+				new Customer(02, "Anuar", "Cleto", "4321"),
+				new Customer(03, "Miguel", "MigelO", "1235"),
+				new Customer(04, "Pedro", "Santo3", "5321")
+				
+			));
+	@GetMapping("Clients")
+	public List<Customer> getCustomers(){
+		return customers;
+	}
+	
+	
+}
